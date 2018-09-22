307afa7ae1c94832620c774266d91622569e3b0b
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-09-22 16:42:37 -0700

    basic functionality working

--- assets/app.js
+++ assets/app.js
@@ -10,17 +10,80 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database()
-var name;
-var destination;
-var firstTrain;
-var frequency;
 
 $("#add-train").click(function(event){
     event.preventDefault();
 
-    name = $("#name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTrain = $("#first-train-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
-    console.log(name, destination, firstTrain, frequency);
+    var trainName = $("#name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var firstTrainTime = $("#first-train-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        firstTrain: firstTrainTime,
+        frequency: trainFrequency
+    }
+
+    //uploads train info to database
+    database.ref().push({
+        name: trainName,
+        destination: trainDestination,
+        firstTrain: firstTrainTime,
+        frequency: trainFrequency
+    });
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+
+    alert("Train added")
+
+    // Clears all of the text-boxes
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
+});
+
+// Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().firstTrain;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    // Employee Info
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(firstTrainTime);
+    console.log(trainFrequency);
+
+    // // Prettify the employee start
+    // var empStartPretty = moment(empStart).format("MM/DD/YYYY");
+    // console.log('Start Date', empStartPretty)
+    // // Calculate the months worked using hardcore math
+    // // To calculate the months worked
+    // var empMonths = moment().diff(moment(empStart), "months");
+    // console.log(empMonths);
+
+    // // Calculate the total billed rate
+    // var empBilled = empMonths * empRate;
+    // console.log(empBilled);
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestination),
+        $("<td>").text(firstTrainTime),
+        $("<td>").text(trainFrequency)
+    );
+
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
 });
